8667e17f51cd7080f77dfc787ae715e89f58daa0
giulia.domingos@programmers.com.br <giulia.domingos@programmers.com.br>
2026-02-07 16:50:09 +0000

    feat: fixes

--- ADB/3. gold schema/ProfitabilityRevenueXCostViewForDash.py
+++ ADB/3. gold schema/ProfitabilityRevenueXCostViewForDash.py
@@ -6,14 +6,21 @@
 # MAGIC   SELECT MAX(Year) AS max_year
 # MAGIC   FROM adventure_works_catalog.gold.vwgoliveprofitability
 # MAGIC )
-# MAGIC
 # MAGIC SELECT
 # MAGIC     ProductName,
+# MAGIC     CONCAT(
+# MAGIC         UPPER(LEFT(ProductName, 1)),                
+# MAGIC         REGEXP_EXTRACT(ProductName, '(\\d+)', 1),   
+# MAGIC         '-',
+# MAGIC         REGEXP_EXTRACT(ProductName, '(\\d+)$', 1)  
+# MAGIC     ) AS ProductShortName,
+# MAGIC
 # MAGIC     SUM(Revenue) AS TotalRevenue,
 # MAGIC     SUM(Cost)    AS TotalCost
+# MAGIC
 # MAGIC FROM adventure_works_catalog.gold.vwgoliveprofitability v
 # MAGIC CROSS JOIN max_year m
-# MAGIC WHERE v.Year >= m.max_year - 1   -- últimos 2 anos
+# MAGIC WHERE v.Year >= m.max_year - 1   
 # MAGIC GROUP BY ProductName
 # MAGIC ORDER BY TotalRevenue DESC
 # MAGIC LIMIT 10;
